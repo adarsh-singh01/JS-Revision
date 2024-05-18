c3bfa50138728d0456ab0370ca64150b246ebd96
Adarsh Singh <adarshsince2004@gmail.com>
2024-05-19 00:56:18 +0530

    comparision operators

---
+++ basic/comparision.js
@@ -0,0 +1,10 @@
+console.log(2==1)
+console.log(2!=1)
+console.log("2">3)//automatically change "2" into Number
+console.log("02"<3)//automatically change "2" into Number
+console.log(null>0);//false
+console.log(null==0)//false
+console.log(null>=0)/*true...coz an equality check and comparision work diffrently...
+comparision convert null to number(here,0)*/
+console.log("3"==3)
+console.log("3"===3)//false as strict check is there.
