2263c636ee841491b791cde94788c32ef661f25e
Adarsh Singh <adarshsince2004@gmail.com>
2024-05-21 00:48:50 +0530

    while,do while

---
+++ iterations/two.js
@@ -0,0 +1,22 @@
+//while
+let i = 0
+while (i<=10) {
+    
+    //console.log(`val of i is ${i}`)
+    i=i+2
+}
+
+let myArr=["jojo","mojo","koko"]
+let index=0
+while (index<myArr.length) {
+    console.log(`name is ${myArr[index]}`)
+    index=index+1
+}
+
+let score=11
+
+do{
+    //ye ek baar hoke rahega
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10)
