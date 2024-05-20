d8388b324a1c01b0b5ad475449b625bd8706f272
Adarsh Singh <adarshsince2004@gmail.com>
2024-05-20 12:44:10 +0530

    more in scope and closure

--- basic_2/scope_1.js
+++ basic_2/scope_1.js
@@ -13,3 +13,33 @@ console.log(c)
 //scope in node environment and in windows/browser is different
 
 //{} is used to declare object but in functions,if elses,it is scope
+
+//----------------------------
+
+function one(){//closure example maybe
+    const username="adarsh"//global...parent cant take icecream from child
+    function two(){
+        const website="reddit"//local...child can take icecream from parent
+        console.log(username)
+    }
+    //console.log(website)//it'll give error
+    two()
+}
+
+one()
+
+
+//+++++++++Interesting+++++++++
+
+
+console.log(addOne(5))//accessing before declaring...it works fine
+function addOne(num){
+    return num+1
+}
+
+
+//console.log(addTwo(8))//it give error bcoz it func is stored in a variable
+const addTwo=function(num){//addTwo is also a function but sometimes called an expression
+    return num+2
+}
+//this is maybe called hoisting
