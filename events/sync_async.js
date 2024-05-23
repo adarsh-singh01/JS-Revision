615400f8719075df9418377b85ddf6017ff39462
Adarsh Singh <adarshsince2004@gmail.com>
2024-05-23 14:46:16 +0530

    sync async theory

---
+++ events/sync_async.js
@@ -0,0 +1,9 @@
+//JS is a Sync language,single threaded
+
+//in executiom conetxt...we execute one line at a time
+
+//Blocking code==>block the flow of program/tum khadde raho me ani leke aata hu....Read file sync
+
+//Non Blocking code ==>does not blocl execution/Tum pjone chalao ...mai aata hu pani leke......read file sync
+
+//BOTH METHODS ARE GOOD BASED ON YOUR USE CASE
