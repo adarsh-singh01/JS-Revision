62e35a277a16763344de78a6c1ad29242d697c66
Adarsh Singh <adarshsince2004@gmail.com>
2024-06-26 14:48:29 +0530

    getset lexical closure

--- classes_oop/getter_setter.js
+++ classes_oop/getter_setter.js
@@ -5,17 +5,18 @@ class User {
     }
 
     get email(){
+        //return this.email.toUpperCase()//Agar variable name same hoga to call stack full ho jayegi
         return this._email.toUpperCase()
     }
     set email(value){
-        this._email = value
+        this._email = value//Agar variable name same hoga to call stack full ho jayegi isliye underscore lga diya bs name different rakhne k liye
     }
 
-    get password(){
+    get password(){//koi agr puche password kya hai to ye encrypted return hoga
         return `${this._password}hitesh`
     }
 
-    set password(value){
+    set password(value){//ye DB me save hoga
         this._password = value
     }
 }
